aa2c8a4db5f764cafc138f0caee4958c642ef433
hyun <rlaalgus132@naver.com>
2019-12-15 12:24:03 +0900

    EditText 타이핑 이벤트 체험   - 비번의 길이에 따라 다른 결과를 출력하는 코드 작성

--- app/src/main/java/com/example/a20191215_02_signupproject/BaseActivity.kt
+++ app/src/main/java/com/example/a20191215_02_signupproject/BaseActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 abstract class BaseActivity: AppCompatActivity() {
     val mContext = this
 
-    abstract fun setupEvent()
-    abstract fun setValue()
+    abstract fun setupEvents()
+    abstract fun setValues()
 
 }
--- app/src/main/java/com/example/a20191215_02_signupproject/SignUpActivity.kt
+++ app/src/main/java/com/example/a20191215_02_signupproject/SignUpActivity.kt
@@ -1,20 +1,51 @@
 package com.example.a20191215_02_signupproject
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : BaseActivity() {
     var lastBackPressed = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        setupEvents()
+        setValues()
     }
 
-    override fun setupEvent() {
+    override fun setupEvents() {
 
-        lastBackPressed = 10L
+//        pwEdt.addTextChangedListener(object : TextWatcher{
+//            override fun afterTextChanged(s: Editable?) {
+//                Log.d("입력된값", s.toString())
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//            }
+//
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//
+//            }
+//
+//        })
+
+        pwEdt.addTextChangedListener {
+            val inputStr = it.toString()
+            if(inputStr.length == 0){
+                pwStatusTxt.text = "비밀번호가 입력되지 않았습니다."
+            }else if(inputStr.length < 5 ){
+                pwStatusTxt.text ="입력된 비번이 너무 짧습니다."
+            }else{
+                pwStatusTxt.text="사용해도 좋은 비밀번호입니다."
+            }
+        }
     }
 
-    override fun setValue() {
+    override fun setValues() {
 
     }
 
