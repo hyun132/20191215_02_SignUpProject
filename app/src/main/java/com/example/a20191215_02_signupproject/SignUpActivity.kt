9aa2128798ed582ffe176e4d480c166921b3604f
hyun <rlaalgus132@naver.com>
2019-12-15 12:11:00 +0900

    스피너 간단하게 사용하기

--- app/src/main/java/com/example/a20191215_02_signupproject/SignUpActivity.kt
+++ app/src/main/java/com/example/a20191215_02_signupproject/SignUpActivity.kt
@@ -1,21 +1,23 @@
 package com.example.a20191215_02_signupproject
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : BaseActivity() {
+class SignUpActivity : BaseActivity() {
+    var lastBackPressed = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
     }
 
     override fun setupEvent() {
 
+        lastBackPressed = 10L
     }
 
     override fun setValue() {
 
     }
+
 
 
 }
